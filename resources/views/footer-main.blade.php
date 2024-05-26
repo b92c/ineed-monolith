778d0bde05a826671d3419559e3a6232cfd76032
Bbarbosa7 <brayann.wheberth@gmail.com>
2024-05-25 22:52:28 -0300

    feat: remove php version on footer

--- resources/views/footer-main.blade.php
+++ resources/views/footer-main.blade.php
@@ -4,6 +4,5 @@
     <br>
     &copy; {{ date('Y') }} Built with pride by <a href="https://github.com/Bbarbosa7" target="_blank"
         class="text-sky-800 hover:underline">Bbarbosa7</a>
-    supported by PHP v{{ PHP_VERSION }}
 </footer>
  
